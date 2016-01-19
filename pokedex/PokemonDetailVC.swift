879759af7709f9f9b345656a05d362197d56a3d1
Shane Lupton <shanelupton@Shanes-MacBook-Pro.local>
2016-01-19 16:08:10 -0700

    added details VC and music.

---
+++ pokedex/PokemonDetailVC.swift
@@ -0,0 +1,41 @@
+//
+//  PokemonDetailVC.swift
+//  pokedex
+//
+//  Created by Shane Lupton on 1/19/16.
+//  Copyright © 2016 Shane Lupton. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    
+    var pokemon: Pokemon!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameLbl.text = pokemon.name
+        
+
+
+    }
+
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
