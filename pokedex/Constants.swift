f9213f84a8761c7053a1ca117b7b37522db11ad9
Shane Lupton <shanelupton@Shanes-MacBook-Pro.local>
2016-01-19 17:59:32 -0700

    Fixed storyboard and added api controls

---
+++ pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Shane Lupton on 1/19/16.
+//  Copyright © 2016 Shane Lupton. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
--- pokedex/Pokemon.swift
+++ pokedex/Pokemon.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Alamofire
 
 class Pokemon {
     private var _name: String!
@@ -17,8 +18,8 @@ class Pokemon {
     private var _height: String!
     private var _weight: String!
     private var _attack: String!
-    private var nextEvolution: String!
-    
+    private var _nextEvolution: String!
+    private var _pokemonUrl: String!
     
     var name: String {
         return _name
@@ -31,6 +32,59 @@ class Pokemon {
     init(name: String, pokedexId: Int) {
         self._name = name
         self._pokedexId = pokedexId
+        
+        _pokemonUrl = "\(URL_BASE)\(URL_POKEMON)\(self._pokedexId)/"
     }
+    
+    func downloadPokemonDetails(completed: DownloadComplete) {
+        
+        let url = NSURL(string: _pokemonUrl)!
+        Alamofire.request(.GET, url).responseJSON{ (response) -> Void in
+            let result = response.result
+            
+            if let dict = result.value as? Dictionary<String, AnyObject> {
+                
+                if let weight = dict["weight"] as? String {
+                    self._weight = weight
+                }
+                
+                if let height = dict["height"] as? String {
+                    self._height = height
+                }
+                
+                if let attack = dict["attack"] as? Int{
+                    self._attack = "\(attack)"
+                }
+                
+                if let defense = dict["defense"] as? Int {
+                    self._defense = "\(defense)"
+                }
+                
+                if let types = dict["types"] as? [Dictionary<String, String>] where types.count > 0 {
+                    
+                    if let name = types[0]["name"] {
+                        self._type = name
+                    }
+                    
+                    if types.count > 1 {
+                        
+                        for var x = 1; x < types.count; x++ {
+                            if let name = types[x]["name"] {
+                                self._type! += "/\(name)"
+                            }
+                        }
+                    }
+                } else {
+                    self._type = ""
+                }
+            }
+        }
+        
+    }
+    
+    
+
+    
+    
     
 }
